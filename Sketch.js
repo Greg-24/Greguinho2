a1b1a8a69a3ff31b0d1d306b6265a2402e2e5a6e
Greg-24 <Dhogenis.godoy@escola.pr.gov.br>
2024-08-19 16:20:38 -0300

    adicionando arquivos js e css

--- Sketch.js
+++ Sketch.js
@@ -1,16 +1,16 @@
 function setup() {
-    createCanvas(600, 600);
-    background("rgb(0,255,158)");
-  }
+  createCanvas(600, 600);
+  background("rgb(0,255,158)");
+}
+
+function draw() {
   
-  function draw() {
-    
-    stroke("red");
-    fill("purple");
-    
-    // console.log(mouseIsPressed)
-    
-    if (mouseIsPressed) {
-      rect(mouseX, mouseY, 40, 65);
-    }
+  stroke("red");
+  fill("purple");
+  
+  // console.log(mouseIsPressed)
+  
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 40, 65);
   }
+}
